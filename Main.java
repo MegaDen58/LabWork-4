c551862dab5b88c836a6ac50207194c14e5612d7
MegaDen58 <96517069+MegaDen58@users.noreply.github.com>
2022-02-25 11:55:34 +0300

    Update Main.java
    added average value

--- Main.java
+++ Main.java
@@ -9,6 +9,8 @@ public class Main {
     public static void main(String[] args) {
         int[] array = new int[] {14, 43, 4, 9, 3, 31, 2, 13, 3, 4, 11, 54, 3, 55, 7};
         minValue(array);
+        averageValue(array);
+
     }
     static void minValue(int[] array) {
         int min = array[0];
@@ -18,5 +20,14 @@ public class Main {
             }
         }
         System.out.println("Минимальное значение = " + min);
+    }
+    static void averageValue(int[] array){
+        int sum = 0;
+        int average = 0;
+        for (int i = 0; i <= (array.length - 1); i++){
+            sum += array[i];
+            average = sum / array.length;
+        }
+        System.out.println("Среднее арифметическое = " + average);
     }
 }
